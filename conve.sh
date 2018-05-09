338ec562ba0aab37e5d241ea63753126554b436e
nikolay <artelbear@gmail.com>
2018-05-09 13:46:34 +0300

    pandoc temp, upgrade convertor

--- conve.sh
+++ conve.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo
+echo STARTING CONVERT...
+echo
+
 for OUTPUT in $(ls md)
 do
-	pandoc -s ./md/$OUTPUT -o ./docx/$OUTPUT.docx
+	pandoc -s ./md/$OUTPUT -o ./docx/$OUTPUT.docx --reference-doc=./template.docx
+	echo OK: $OUTPUT
 done
+
+echo
+echo OK
+echo
+
+echo Ave Kiselev Nikolay
--- downmark.sh
+++ downmark.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo
+echo STARTING CONVERT...
+echo
+
 for OUTPUT in $(ls md)
 do
 	pandoc -s ./md/$OUTPUT -o ./md/$OUTPUT --wrap=none
+	echo OK: $OUTPUT
 done
+
+echo
+echo OK
+echo
+
+echo Ave Kiselev Nikolay
